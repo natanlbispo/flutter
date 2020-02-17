452bcd33493b55f2aec759053a5b381d1856538c
root <root@LAPTOP-Q8OSRTDP.localdomain>
2020-02-17 17:01:18 -0300

    #1 Primeiro APP - Frases do Dia - Completo

--- udemy_01/lib/main.dart
+++ udemy_01/lib/main.dart
@@ -1,61 +1,75 @@
-import 'package:flutter/material.dart';
-/*
-  Stateless -> Widgets que não podem ser alterados(constantes)
-  Stateful -> widgets que podem ser alterados (variáveis)
-*/
+import 'dart:math';
+import "package:flutter/material.dart";
 
-void main() {
-  runApp(new MaterialApp(
-    debugShowMaterialGrid: false,
-    home: HomeStateful(),
-  ));
+void main() => (runApp(MaterialApp(
+      home: FrasesDoDia(),
+      debugShowCheckedModeBanner: false,
+    )));
+
+class FrasesDoDia extends StatefulWidget {
+  @override
+  _FrasesDoDiaState createState() => _FrasesDoDiaState();
 }
 
-class HomeStateful extends StatefulWidget {
-  @override
-  _HomeStatefulState createState() => _HomeStatefulState();
-}
+class _FrasesDoDiaState extends State<FrasesDoDia> {
+  var _frases = [
+    "Frase 1",
+    "Frase 2",
+    "Frase 3",
+    "Frase 4",
+  ];
 
-class _HomeStatefulState extends State<HomeStateful> {
+  var _padrao = "Clique Aqui";
+
+  int _gerarFrase() {
+    var num = new Random().nextInt(_frases.length);
+    return num;
+  }
+
   @override
-  var _texto = "Test";
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("App bar",
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blueGrey)),
-        backgroundColor: Colors.lightBlueAccent,
+        title: Text("Frase do Dia", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.lightBlue,
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () {
-                print("Clicou BÊ");
-                setState(() {
-                  _texto = "Curso Flutter";
-                });
-                // _texto = "Curso Flutter";
-              },
-              color: Colors.blueGrey,
-              child: Text("Clique Aqui"),
-            ),
-            Text("Nome: $_texto")
-          ],
-        ),
+      body: Center(
+        child: Container(
+            padding: EdgeInsets.all(16),
+            // width: double.infinity,
+            /*decoration: BoxDecoration(
+              border: Border.all(width: 3, color: Colors.redAccent),
+            ),*/
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset("images/logo.png"),
+                Text(
+                  _padrao,
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                RaisedButton(
+                  child: Text("Nova Frase",
+                      style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold)),
+                  color: Colors.blue,
+                  onPressed: () {
+                    var num = _gerarFrase();
+                    setState(() {
+                      _padrao = _frases[num];
+                    });
+                  },
+                ),
+              ],
+            )),
       ),
     );
   }
 }
-
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var _titulo = "App bar";
-  }
-}
-
-class FraseDoDia {}
