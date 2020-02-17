a985b8f95c1ad1bbae6dfd6acb6c11304973bdac
root <root@LAPTOP-Q8OSRTDP.localdomain>
2020-02-17 14:22:58 -0300

    Learning Scafold

--- udemy_01/lib/main.dart
+++ udemy_01/lib/main.dart
@@ -2,15 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: 'Frase do Dia',
-    home: Row(
-      children: <Widget>[
-        Text(" T1 "),
-        Column(children: <Widget>[Text('Ts1'), Text('Ts2')]),
-        Text(' T3 '),
-      ],
+    debugShowMaterialGrid: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("App bar",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.blueGrey)),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Text("T1"),
+            Text("T1"),
+          ],
+        ),
+        padding: EdgeInsets.all(120),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.lightBlue,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            children: <Widget>[
+              Text(
+                "bottom app bar",
+                style: TextStyle(
+                    color: Colors.blueGrey, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
     ),
-    // home: Container(color: Colors.amber),
   ));
 }
 
